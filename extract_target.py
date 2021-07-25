ec634fcbaa90593b7ebdacb83fa9cbb40e75cd25
aeea-0605 <tmdwnrj96@gmail.com>
2021-07-25 17:41:39 +0900

    modify path

--- extract_target.py
+++ extract_target.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import cv2
 
-src = cv2.imread('/Users/aeea/Desktop/git/project/side-dl-project/test_imgs/1-20.png')
+src = cv2.imread('/Users/aeea/Desktop/git/project/obj-detect-repo/test_target_imgs/target_2-208.png')
 if src is None:
     print('Image load failed')
     sys.exit()
@@ -14,7 +14,7 @@ target_img = src[rc[1]:rc[1]+rc[3], rc[0]:rc[0]+rc[2]]
 
 
 # target image 저장
-cv2.imwrite('/Users/aeea/Desktop/git/project/side-dl-project/test_target_imgs/test.png', target_img)
+cv2.imwrite('/Users/aeea/Desktop/git/project/obj-detect-repo/extracted_target_imgs/null.png', target_img)
 cv2.imshow('target', target_img)
 
 cv2.waitKey()
